2b7cbcedb15601fe651b5adf73333bc8c81085ac
Alexander Fletcher <alexander.fletcher@vgw.co>
2019-07-09 17:59:46 +0800

    string literal and print tokemns working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use std::fs;
+mod tokenizer;
 fn main() {
-    println!("Hello World");
+    println!("Hello, World!");
+    let filename = "examples/hello_world.slam";
+    let contents = fs::read_to_string(filename).expect("something went wrong reading the file");
+    tokenizer::tokenize(contents);
 }
